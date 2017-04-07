000149959d1fb105905010615720d4aaf7f5070b
Selom Viadenou <junior@MacBook-Pro-de-Selom.local>
2017-04-08 00:03:23 +0200

    test

--- main.cpp
+++ main.cpp
@@ -8,15 +8,6 @@ using namespace std;
 
 int main() {
 
-    Hackathon hackathon;
-    Etape etape;
-    etape.ordre = 1;
-
-    hackathon.etapes.push_back(etape);
-
-    cout << hackathon.etapes[0].ordre << endl;
-
-
 
     return 0;
 }
